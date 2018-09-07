010bc29009541bc418a25900a2ebfaa258fb9bf3
gingkasina <60030007@kmitl.ac.th>
2018-09-07 14:53:04 +0700

    08

--- Lab01/Lab01/Program.cs
+++ Lab01/Lab01/Program.cs
@@ -7,13 +7,7 @@ namespace Lab01
     {
         static void Main(string[] args)
         {
-            const double i = 123.456789d;
-            Console.WriteLine("{0:F1}", i);
-            Console.WriteLine("{0:F2}", i);
-            Console.WriteLine("{0:F3}", i);
-            Console.WriteLine("{0:F4}", i);
-            Console.WriteLine("{0:F5}", i);
-
+            Console.WriteLine("{0:#.###}.", 1234.56789);
 
         }
     }
