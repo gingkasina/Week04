c4dc879bc8a9080b3e45266947dc82293886543d
gingkasina <60030007@kmitl.ac.th>
2018-09-14 14:18:19 +0700

    W04P11

--- Lab01/Lab01/Program.cs
+++ Lab01/Lab01/Program.cs
@@ -7,10 +7,17 @@ namespace Lab01
     {
         static void Main(string[] args)
         {
-            string str;
-            Console.Write("Enter some characters.");
-            str = Console.Read();
-            Console.WriteLine("You entered: " + str);
+            string n, s;
+            double id, gpa;
+            Console.Write("Name : ");
+            n = Console.ReadLine().ToString().ToUpper();
+            Console.Write("Last Name : ");
+            s = Console.ReadLine().ToString().ToUpper();
+            Console.Write("ID : ");
+            id = double.Parse(Console.ReadLine());
+            Console.Write("GPA : ");
+            gpa = double.Parse(Console.ReadLine());
+               
 
         }
     }
